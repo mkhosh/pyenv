f5ab1bc26ebcf0427e2681134d25f4cc5d9b9253
Moe Khoshneshin <mkhoshneshin@apple.com>
2016-08-24 10:54:19 -0700

    add kernel and ability to run notebook

--- pyenv.sh
+++ pyenv.sh
@@ -3,9 +3,12 @@
 virtualenv --no-site-packages .pyenv
 
 source ./.pyenv/bin/activate
+pip install -U ipykernel
 pip install -U jupyter
 pip install -I ipython==4.2.1
 pip install -U numpy
 pip install -U scipy
 pip install -U sklearn
 pip install -U matplotlib
+
+ipython kernel install --user
